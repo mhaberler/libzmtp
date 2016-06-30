e400c7873cdbc55639988e3f7046cea136ed6701
Michael Haberler <git@mah.priv.at>
2016-06-30 14:14:38 +0200

    testxsub.c: example

---
+++ src/testxsub.c
@@ -0,0 +1,40 @@
+#include "zmtp_classes.h"
+
+unsigned short port = 4712;
+char *uri= "127.0.0.1";
+int count = 3;
+char *topic = "\001topic";
+
+int main (int argc, char *argv [])
+{
+    int rc;
+    int flags;
+
+    zmtp_dealer_t *dealer = zmtp_dealer_new ();
+    assert(dealer != NULL);
+    rc = zmtp_dealer_tcp_connect (dealer, uri, port);
+    assert(rc == 0);
+
+    // subscribe to 'topic'
+    zmtp_msg_t *tx = zmtp_msg_from_const_data (0, topic, strlen(topic));
+    assert (tx);
+    rc = zmtp_dealer_send (dealer, tx);
+    assert(rc == 0);
+    zmtp_msg_destroy (&tx);
+
+    for (int i = 0; i < count; i++) {
+	size_t size;
+	zmtp_msg_t *rx;
+	do {
+	    rx = zmtp_dealer_recv (dealer);
+	    assert(rx != 0);
+	    size = zmtp_msg_size (rx);
+	    flags = zmtp_msg_flags (rx);
+	    printf("got %zu: '%.*s' flags=%d\n",
+		   size, (int) size,
+		   zmtp_msg_data (rx), flags);
+	} while (flags & ZMTP_MSG_MORE);
+	zmtp_msg_destroy (&rx);
+    }
+    return 0;
+}
